a1d8cb6759916c8e4d85e24a367bc9223e2dcb1f
LynaSovann <sovannlyna2004@gmail.com>
2025-08-06 15:26:52 +0700

    update

---
+++ src/main/java/com/java/spring/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.java.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    @GetMapping
+    public String getUsers() {
+        return "This is a user controller";
+    }
+}
